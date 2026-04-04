3b7f241dbd982122d212f5a0e499207c42385fd9
hackvyshu-debug <vyshnavas840@gmail.com>
2026-04-04 09:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menu = document.getElementById("menu");
-const navLinks = document.getElementById("navLinks");
 
-menu.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
